7031239137ba45ea11009a0749dcb06e7d73e6b7
Lorenzo Limoli <lorenzo.limoli@outlook.it>
2020-06-15 19:02:49 +0200

    LWF TEST

--- libs/models/lwf.py
+++ libs/models/lwf.py
@@ -1,4 +1,8 @@
+from typing import Iterator
+
 import torch.nn as nn
+from torch.nn import Parameter
+
 from libs.resnet import resnet32
 import copy
 from libs.utils import get_one_hot
@@ -8,6 +12,7 @@ class LwfModel(nn.Module):
 
     def __init__(self, num_classes=10):
         super(LwfModel, self).__init__()
+        self.num_classes = num_classes
         self.known_classes = 0
         self.old_net = None
         self.net = resnet32(num_classes=num_classes)
@@ -21,7 +26,7 @@ class LwfModel(nn.Module):
 
     def after_train(self, class_batch_size):
         self.known_classes += class_batch_size
-        self.old_net = copy.deepcopy(self.net)
+        self.old_net = copy.deepcopy(self)
 
     def increment_class(self, num_classes=10):
         weight = self.net.fc.weight.data
@@ -34,23 +39,23 @@ class LwfModel(nn.Module):
         self.net.fc.bias.data[:out_feature] = bias
 
     def forward(self, x):
-        print("******************************** FORWARD CHIAMATO ******************************")
         return self.net(x)
 
-    def compute_distillation_loss(self, images, labels, new_outputs, step, device, num_classes=10):
-        if step <= 0:
-            raise ValueError('step must be >= 1')
+    def compute_distillation_loss(self, images, labels, new_outputs, device, num_classes=10):
 
-        if step == 1:
-            return self.bce_loss(new_outputs, get_one_hot(labels, num_classes, device))
+        if self.known_classes == 0:
+            return self.bce_loss(new_outputs, get_one_hot(labels, self.num_classes, device))
 
         sigmoid = nn.Sigmoid()
         n_old_classes = self.known_classes
-        n_new_classes = self.known_classes + num_classes
+        # n_new_classes = self.known_classes + num_classes
         old_outputs = self.old_net(images)
 
-        targets = get_one_hot(labels, n_new_classes, device)
-        targets[:, :n_old_classes] = sigmoid(old_outputs)
+        targets = get_one_hot(labels, self.num_classes, device)
+        targets[:, :n_old_classes] = sigmoid(old_outputs)[:, :n_old_classes]
         tot_loss = self.bce_loss(new_outputs, targets)
 
         return tot_loss
+
+    def parameters(self, recurse: bool = ...) -> Iterator[Parameter]:
+        return self.net.parameters()
